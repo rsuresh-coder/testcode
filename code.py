924c9d3987da0150aaf7376c11f309bf9173172f
rsuresh-coder <82728778+rsuresh-coder@users.noreply.github.com>
2024-08-26 13:55:13 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,57 +1,218 @@
-from typing import Dict, TypedDict, Optional
-from langgraph.graph import StateGraph, START, END
-from PIL import Image
-import io
- 
-class GraphState(TypedDict):
-    question: Optional[str] 
-    search_results: Optional[str] 
-    response: Optional[str]
+question: Why did I receive a credit card?
+answer: The credit card from Wells Fargo Bank, N.A. is associated with the account you applied for when you recently financed a purchase. Use this card as a convenient way to make future purchases at participating businesses on the credit card program. If you didn't apply for a credit card account with Wells Fargo Bank, N.A., please contact us at 1-877-805-7744. For your protection, sign the back of your credit card and keep it in a secure location. If you haven't received your card within 7 – 10 days of being approved for credit, call 1-877-805-7744, Monday – Friday, 8:00 am to 6:00 pm Central Time.
 
-rag_graph = StateGraph(GraphState)
-  
-def handle_search_node(state):
-    question = state.get('question', '').strip()
-    #Perform search here 
-    return {"search_results": ["Result 1 " + question, "Result 2 " + question] }
+question: Is this a one-time loan?
+answer: No, this is a credit card account financed through Wells Fargo Bank, N.A., and monthly payments are required as long as you have an account balance. You can use your available credit over and over at participating businesses.
 
-def handle_generation_node(state):
-    search_results = state.get('search_results')
-    generated = ','.join(search_results)
-    return {"response": generated}
+question: What is the difference between this card and a general-purpose credit card?
+answer: This credit card, issued through Wells Fargo Bank, N.A., is used for financing purchases at participating businesses that may offer special financing options. A general purpose credit card can be used for multiple types of purchases anywhere that card is accepted, but it generally doesn't offer special financing options. Please review your statement and materials that came with your credit card if you'd like more information.
 
-#Add Nodes
-rag_graph.add_node("search", handle_search_node)
-rag_graph.add_node("generate", handle_generation_node)
+question: Where can I use this card?
+answer: You can use your card again where you opened your account. For certain credit card programs, you may be able to find other businesses where you can use your card to finance additional products and services. Please review your statement and materials that came with your credit card for more details.
 
-#Add Edges
-rag_graph.add_edge('search', "generate")
-rag_graph.add_edge('generate', END)
+question: Are there additional benefits of this card that I should be aware of?
+answer: Yes! Eligible Wells Fargo Online® customers get free access1 to Credit Close-Up® which includes: Learn more
 
-#Set Entry point
-rag_graph.set_entry_point("search")
+question: When do my promotional terms end?
+answer: It depends on the type of financing promotion you have, but the date your special terms end is shown on your statement. Remember, reduced rate and 0% APR promotions apply to your financed purchase until the balance is paid in full. If you financed with a No Interest if Paid in Full promotion, interest accrues on the purchase balance at the regular APR for Purchases from the date of purchase on any outstanding balance. Paying only the minimum monthly payment will not pay off the purchase balance before the end of the promotional period. But, you won't pay any interest charges as long as you pay the purchase balance in full within the promotional period.
 
-app = rag_graph.compile()
+question: What does Regular Terms mean, and when does it apply?
+answer: Regular terms means the Annual Percentage Rate (APR) for Purchases stated in your cardholder terms and conditions and will apply toward:
 
-try:
-    image_bytes = app.get_graph().draw_mermaid_png()
-    image = Image.open(io.BytesIO(image_bytes))
-    image.save("graph.png", "PNG")
-except Exception as e:
-    print(e)
-    pass
-  
-while True:
-    user_input = input("User: ")
-    if user_input.lower() in ["quit", "exit", "q"]:
-        print("Goodbye!")
-        break
-    for event in app.stream( {"question" : user_input}):
-        # print(event.keys(), event.values())
-        for val in event.values():
-            if 'response' in val.keys():
-                print(val['response'])
-         
-# inputs = {"question": "Hello, good morning"}
-# result = app.invoke(inputs)
-# print(result)
+question: How do I request a credit limit increase?
+answer: Please call Customer Service at 1-877-805-7744 to request a higher credit limit.
+
+question: Why is my account closed?
+answer: Please call Customer Service at 1-877-805-7744.
+
+question: Why did I get a call about my account?
+answer: We call cardholders to verify that their financed product or service has been received, to make sure customers are satisfied with their purchase, and to confirm that their account has been charged correctly. Please call us back at 1-800-378-6755, Monday – Friday, 8:00 am to 6:00 pm Central Time to follow up with us. If we don't have your correct mailing address, we may also call you or leave a message. Please return our call at 1-800-378-6755, Monday – Friday, 8:00 am to 6:00 pm Central Time.
+
+question: What should I do if I suspect fraudulent charges on my credit card account?
+answer: We're here to help. Please call us at 1-877-805-7744, Monday - Friday, 8:00 am to 6:00 pm Central Time, and a Customer Service representative will review your credit card account with you.
+
+question: Who do I contact if my card has been lost or stolen?
+answer: As a Wells Fargo customer, we're here to help you. Call our Customer Service department at 1-877-805-7744 so we can review your credit card account with you and guide you through the next steps.
+
+question: Who do I contact if I didn't apply for this credit card?
+answer: Please call Customer Service at 1-877-805-7744 if you received a credit card in the mail but you haven't applied for a credit card account.
+
+question: How do I find my available credit, account balance, or credit limit?
+answer: There are a few different ways you can find this information:
+
+question: How long will my account remain open if I have paid off the balance and have not had any transactions for an extended period of time?
+answer: Generally, your account will remain open for approximately 18 months from the date of last activity. After that, it will be closed due to inactivity.
+
+question: When will I receive my first billing statement?
+answer: Within 45 days after making the first charge to your credit card account. If you'd rather receive Online Statements2 going forward, choose this option at Wells Fargo Online. If you're already set up for an online account, check there to review if a statement has generated. If you don't have an online account, set up one now. If you don't receive your statement in the mail within 45 days, please call us at 1-877-805-7744, Monday - Friday, 8:00 am to 6:00 pm Central Time.
+
+question: Why didn't I receive my billing statement?
+answer: You may not have received a billing statement if: If you think we have an incorrect address, you may
+                update it on Wells Fargo Online or call us at 1-877-805-7744,
+                Monday - Friday, 8:00 am to 6:00 pm Central Time. If you
+                don't receive your statement in the mail within 45 days
+                of your first purchase, please call us at
+                1-877-805-7744. Keep in mind, you won't receive a
+                billing statement until a charge is posted to your
+                account.
+
+question: Can I stop receiving paper statements in the mail?
+answer: Absolutely. You can choose Online Statements2 through Wells Fargo
+                Online. We'll even send you email notifications when
+                your statements are available.
+
+question: How can I get my statements online?
+answer: It's easy. Sign on to Wells Fargo Online or sign up if you haven't already. Then, choose Online Statements2 for your account from the Manage Delivery Preferences page.
+
+question: What if I have questions reading my statement?
+answer: Review this guide to help you understand each part of your statement.
+
+question: Why am I getting my bill from Wells Fargo?
+answer: Since your credit card account is issued through
+            Wells Fargo Bank, N.A., your bills will
+            come from Wells Fargo.
+
+question: How can I receive my statement in Spanish or Braille?
+answer: Please call Customer Service at 1-877-805-7744 to
+            request your statement in Spanish or Braille. Large
+            print, audio statements, and other alternative options
+            are also available.
+
+question: How do I make payments online?
+answer: If you already have a Wells Fargo Online profile with Wells
+            Fargo, your credit card account will be automatically
+            added to your profile. You don't need to set one up for
+            your retail credit card account. If you don't have a Wells Fargo Online profile, you can add it once
+            you have signed on. Sign up for Wells Fargo Online to make one-time
+            online payments that will post the same day they're
+            scheduled or to set up automatic payments. Take the tour to learn more.
+
+question: How do I set up automatic payments?
+answer: You can set up automatic payments through Wells Fargo Online. Or, you may call Customer Service at 1-877-805-7744 to request the enrollment form. It can take 30 - 60 days to process your automatic payment enrollment form. Continue making the minimum payment until your statement reflects that the automatic payment has been set up within the Important Information section on your statement.
+
+question: How long should I allow for my payment to reach you?
+answer: We suggest allowing at least: Wells Fargo Online payments
+            3 - 5 business days using non-Wells Fargo bank accounts
+            1 business day for Wells Fargo Online payments paid using Wells Fargo bank accounts
+            By mail: 7 - 10 business days
+            By phone: Same day (for payments by Midnight,
+            Pacific Time)
+            You can choose to use a bill payment service other than
+            Wells Fargo Online to make payments with us.
+            Please refer to that service provider for the accuracy
+            and timeliness of your online bill payments. For the
+            other payment methods, please refer to the service
+            provider for details. You can choose to use a bill payment service other than
+            Wells Fargo Online to make payments with us.
+            Please refer to that service provider for the accuracy
+            and timeliness of your online bill payments. For the
+            other payment methods, please refer to the service
+            provider for details.
+
+question: What happens if my payment is late?
+answer: Please make sure we receive your payment on or before the due date on your statement. Otherwise, late fees and/or interest may be applied to your account. To avoid late fees, remember:
+
+question: Can I change my payment due date?
+answer: Yes, you may be able to change the date your minimum
+            payment is due at Wells Fargo Online or by calling Customer Service at
+            1-877-805-7744. Some limitations apply. Please note: You may change your
+            payment due date once every 12 months, but the 3, 6, 10, 15, 19, 24, 26, 29, 30, and 31 of any month aren't available to select. It can take up to two billing cycles to process the due date change. You still need to pay your minimum payment by the due date reflected on your statement until the change is processed.
+
+question: Can I pay ahead on my account?
+answer: If you want to pay more than the minimum payment, you can do that any time, but you must make at least the
+            minimum payment every billing cycle. For example, if your minimum payment amount is $40 and
+            you pay $80 by the due date in January, that amount won't cover both January and February minimum payments.
+            You must still make the $40 minimum payment by the
+            February due date.
+
+question: How is my payment applied?
+answer: Your payment applies first to the minimum payment due (including past due amounts) for that billing cycle. Anything over the minimum payment due generally applies first to No Interest if Paid in Full (deferred interest) promotions in the last two billing cycles before the expiration date of the promotion (when applicable); then to regular term balances with the highest APR to the lowest APR; and then to all other sub-accounts based on expiration date. If you'd like to know how payments apply to your specific account, call us at 1-877-805-7744, Monday - Friday, 8:00 am to 6:00 pm Central Time.
+
+question: Can I pay off my credit card account early without a pre-payment penalty?
+answer: Yes, you may pay your outstanding balance in full at
+            any time without penalty.
+
+question: Can I pay my bill with another credit/debit card?
+answer: No, we don't accept payments using credit or debit
+            cards. Refer to the payment options that are available.
+
+question: Can I make a payment from more than one bank account?
+answer: Yes. When you schedule payments through Wells Fargo Online, you may add additional bank
+            accounts that are in your name. You may also call
+            Customer Service at 1-877-805-7744.
+
+question: What is Wells Fargo Online?
+answer: It's Wells Fargo's online banking system where you can
+            securely access and manage your Wells Fargo
+            accounts, pay your bills, review your statements2, and much
+            more. Take the tour of Wells Fargo Online.
+
+question: How do I sign up for Wells Fargo Online?
+answer: If you already have a Wells Fargo Online
+ profile, your credit card account will be automatically added to your profile. Or, you can add it once you have signed on. Sign up for Wells Fargo Online to review and
+            manage your personal and business accounts securely
+            online, anywhere or anytime you have internet access.
+            You'll need your Social Security Number/ Individual Tax Identification Number ITIN, Wells Fargo Bank, N.A. credit card account number, and
+            email address. After answering a few questions during
+            the sign-up process, you'll have an opportunity to
+            create a username and password. Once you have created a username and password, you can sign on to Wells Fargo Online from your
+            computer, mobile device, or tablet to access your
+            eligible Wells Fargo accounts. In some cases, we may
+            need to contact you by phone to complete the sign-up
+            process. Once successfully signed up, you will receive
+            confirmation by email or U.S. Mail.
+
+question: Can I use the same account that is set up for other Wells Fargo products?
+answer: Yes, if you already have a Wells Fargo Online
+ profile, your credit card account will be added automatically.
+
+question: What can I do through Wells Fargo Online?
+answer: Access and manage your Wells Fargo accounts, make online payments, and much more. With an online transfer, you can make a one-time, same-day payment, or schedule a future payment to your credit card account. Payments made before Midnight, Pacific Time, are credited to your account on the same day. Or, set up automatic payments. Your payment is automatically deducted from your deposit account. Choose to pay the minimum payment amount, your balance in full, or a specified dollar amount. Payments will be made automatically on your payment due date, starting after you receive notification of your automatic payments on your statement. You may also access your FICO® Credit Score, Wells Fargo credit rating, and more1. Take the tour to review all of the features and tools available.
+
+question: How can I manage my automatic payments?
+answer: With automatic payments, your payment is automatically
+            deducted from your deposit account. Choose
+            to pay the minimum payment amount, your balance in full,
+            or a specified dollar amount. Set up automatic payments through Wells Fargo Online. Or, you may call Customer Service
+            at 1-877-805-7744 to request the enrollment form. It can
+            take 30 - 60 days to process your automatic payment
+            enrollment form. Continue making the
+            minimum payment until your statement reflects that
+            the automatic payment has been set up within the
+            Important Information section on your statement. You may cancel your automatic payments through Wells
+            Fargo Online or by calling Customer Service at
+            1-877-805-7744. To modify your automatic payments,
+            please call Customer Service at 1-877-805-7744. It can
+            take 30 - 60 days to process your new automatic payment
+            enrollment form. Your current automatic payment will
+            stay in effect until your statement reflects that the
+            new automatic payment has been set up within the
+            Important Information section on your statement.
+
+question: When will my automatic payment be debited from my bank account?
+answer: Your automatic payment will be reflected on your account on your due date.
+
+question: What if I forgot my username or password?
+answer: We can remind you of your username, and you can create a new password online. If you're still having trouble signing on, please call Customer Service at 1-877-805-7744
+
+question: How can I check my payment due date, balance, available credit, and other important account information?
+answer: Sign on to Wells
+            Fargo Online to review your account details. Or, open
+            your most recent account statement2. You can find all of
+            your important information there, too. If you're not
+            sure where to look for specific information, this guide can
+            help you understand each part of your statement.
+
+question: How do I update my personal information?
+answer: Sign on to Wells Fargo Online to update your
+            email address, phone numbers, or address.
+
+question: Who should I contact if I have additional questions about my account?
+answer: There's a lot of information about your account on
+            our site. But, if you have account questions you can't
+            find answers to, please call our Customer Service
+            department at 1-877-805-7744 so we can help you.
+
+question: Who should I contact if I need technical assistance?
+answer: If you need help with your online account, you can call Online Customer Service at 1-800-869-3557, 24 hours a day, 7 days a week.
+
