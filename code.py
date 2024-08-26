b1d3d43dcd94fde82d11d918ac01cb5ea53c3400
rsuresh-coder <82728778+rsuresh-coder@users.noreply.github.com>
2024-08-26 16:15:10 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,66 +1,62 @@
-Question: What will I need to enroll?
-Answer: ATM/debit or credit card number or Wells Fargo account or loan number
-Social Security or Tax ID number
-Access to your email
-Your current telephone number
-To learn how we help protect and use your information, read our privacy policies.
+from typing import Dict, TypedDict, Optional
+from langgraph.graph import StateGraph, START, END
+from PIL import Image
+import io
+from langchain_core.runnables.graph import MermaidDrawMethod
+     
+ 
+class GraphState(TypedDict):
+    question: Optional[str] 
+    search_results: Optional[str] 
+    response: Optional[str]
 
-Question: How do I access my Wells Fargo accounts online?
-Answer: You can access it from your desktop or mobile device. Once you've enrolled, you'll receive an email confirmation, and you'll be ready to sign on and manage your accounts.
+rag_graph = StateGraph(GraphState)
+  
+def handle_search_node(state):
+    question = state.get('question', '').strip()
+    #Perform search here 
+    return {"search_results": ["Result 1 " + question, "Result 2 " + question] }
 
-Question: What should I do if my password is suspended?
-Answer: For your security, we may suspend your access to Wells Fargo Online. To regain access, you’ll need to create a new password. For additional protection, we also recommend that you change your username after signing on.
-If you do not have the required information to create a new password through Wells Fargo Online, you will need to contact Online Customer Service at 1-800-956-4442.
+def handle_generation_node(state):
+    search_results = state.get('search_results')
+    generated = ','.join(search_results)
+    return {"response": generated}
 
-Question: How do I change my username and/or password?
-Answer: Sign on, and access Change Username or Change Password from the menu.
-Create a unique (not used anywhere else) username and password for Wells Fargo Online. We recommend you memorize your username and password rather than writing it down.
+#Add Nodes
+rag_graph.add_node("search", handle_search_node)
+rag_graph.add_node("generate", handle_generation_node)
 
-Question: How do I change or add another email address, phone number, or mailing address?
-Answer: Simply sign on to Wells Fargo Online® and access Update Contact Information to review your email addresses, phone numbers, and mailing addresses.
-If your new address is outside of the United States, please contact us at 1-800-956-4442.
-If you receive electronic delivery of brokerage statements and documents, you will need to sign on to Wells Fargo Online. Click the Brokerage tab, and access Document Delivery Preferences under Customer Service to modify the email address to which your notifications are sent.
+#Add Edges
+rag_graph.add_edge('search', "generate")
+rag_graph.add_edge('generate', END)
 
-Question: Is there a fee for Zelle®?
-Answer: Wells Fargo does not charge a fee to send or receive money with Zelle®. However, when using Zelle® on a mobile device, your mobile carrier’s message and data rates may apply.2 Account fees (e.g., monthly service, overdraft) may apply to Wells Fargo account(s) with which you use Zelle®.
+#Set Entry point
+rag_graph.set_entry_point("search")
 
-Question: How much money can I send and receive with Zelle®?
-Answer: Sending limits: For your security, we restrict the amount of money you can send to recipients. There are daily and 30-day (rolling) limits. The amounts you can send daily and over 30 days will vary based on your funding account, your recipient, your account and online banking history, and your payment history for each recipient.
-Receiving limits: We do not limit how much money you can receive with Zelle®. However, the sender may be subject to limits on how much they can send you, based on the policies of their financial institution.
-For more information about Zelle® sending and receiving limits with Wells Fargo, see the Zelle® Transfer Service Addendum. These limits apply only to Zelle® payments when initiated from Wells Fargo Online® or the Wells Fargo Mobile® app.
+app = rag_graph.compile()
 
-Question: How do I send an online wire
-Answer: Sign on to Wells Fargo Online, go to Transfer & Pay, and enroll in Online Wires to get started.
-Add your recipient, select funding account and amount (along with any additional details required for certain countries).
-Review and send your payment.
-You must have a current residence in the United States to send an online wire. Customers living in U.S. territories (American Samoa, Guam, Northern Mariana Islands, Puerto Rico, and the U.S. Virgin Islands) or other countries can’t send wires online.
-
-Question: How do I send money internationally using my ExpressSend service agreement?
-Answer: The first remittance sent under each service agreement must be completed in person at a Wells Fargo branch. Customers with an existing checking or savings account may be able to complete their first remittance by calling the Wells Fargo Phone Bank, subject to caller authentication requirements and additional fraud prevention controls. Additional remittances may be completed at a Wells Fargo branch, by calling the Wells Fargo Phone Bank, or through Wells Fargo Online® at wellsfargo.com. ExpressSend is not available on the Wells Fargo Mobile® app. Customers must use a desktop or laptop to conduct online ExpressSend transfers.
-Customers cannot set up recurring, scheduled remittance transfers; the customer must always initiate each transfer.
-
-Question: How do I get started with Bill Pay?
-Answer: You can access Bill Pay for the first time, from either your desktop or mobile device, if you have a Wells Fargo checking account and are enrolled in Wells Fargo Online. If you don’t have a Wells Fargo Online username and password, enroll now to get started. Then, just sign on to Wells Fargo Online to access Bill Pay.
-
-Question: How do I set up, add, or delete alerts4?
-Answer: Sign on to Wells Fargo Online® and go to Manage Alerts.
-Choose an account for which you want to add or edit alerts.
-Check the boxes to select or de-select specific alerts.
-Enter dollar thresholds and frequency of alerts where appropriate.
-Save your edits.
-
-Question: How do I sign up for Wells Fargo Online® statements5?
-Answer: Wells Fargo Online comes with the ability to receive and view statements for most of your accounts online. Simply enroll to set up a username and password to access your personal and business accounts online.
-If you already have a username and password, sign on to view your statements online. Or, to change how you wish to receive your statements and other documents, access Manage Delivery Preferences from Statements and Documents.
-If I decide later to switch back to paper statements, will I still be able to view historical online statements?
-If you choose “Online & Mail” as your delivery preference, you will continue to have access to both current and historical online statements.
-If you choose “Mail,” you will no longer have access to historical online statements, and your current statements will be delivered to you by mail. Should you need a copy of an eligible historical online statement, you can request a statement copy by calling 1-800-742-4932. Your account may be charged for any applicable fees.
-
-Question: When will I receive my tax documents in the mail?
-Answer: Wells Fargo generally mails original tax documents no later than January 31, or by the IRS deadline of February 15 for brokerage accounts. Depending on the postal service delivery, you can usually expect to receive your tax documents by the third week of February or by the end of February for brokerage accounts. If you don’t receive your tax documents by then, please call us at 1-800-TO-WELLS (1-800-869-3557) or 1-866-281-7436 for brokerage customers.
-
-Question: Can I view my tax documents online?
-Answer: Yes, as long as you are the primary owner of the account, you can view electronic copies of your tax documents through Wells Fargo Online at your convenience. For security reasons, tax documents are available for online viewing only by the owner of the Taxpayer Identification Number (TIN) associated with the IRS reporting. (However, co-borrowers have online access to mortgage and home equity tax documents beginning with the 2015 tax year.) 
-Checking, savings, brokerage, mortgage, home equity, and trust accounts: Sign on to Wells Fargo Online.
-Brokerage customers: You may not have access to all of your brokerage tax documents on Wells Fargo Online. Visit Wells Fargo Advisors for access to all of your brokerage tax documents. Select Statements & Documents, then Tax Docs/1099s.
-
+try:
+    image_bytes = app.get_graph().draw_mermaid_png(draw_method=MermaidDrawMethod.PYPPETEER)
+    image = Image.open(io.BytesIO(image_bytes))
+    image.save("graph.png", "PNG")
+except Exception as e:
+    print(e)
+    pass
+  
+while True:
+    user_input = input("User: ")
+    if user_input.lower() in ["quit", "exit", "q"]:
+        print("Goodbye!")
+        break
+    inputs = {"question": "Hello, good morning"}
+    result = app.invoke(inputs)
+    print(result['response'])
+    # for event in app.stream( {"question" : user_input}):
+    #     # print(event.keys(), event.values())
+    #     for val in event.values():
+    #         if 'response' in val.keys():
+    #             print(val['response'])
+         
+# inputs = {"question": "Hello, good morning"}
+# result = app.invoke(inputs)
+# print(result)
