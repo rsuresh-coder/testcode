e560b130ed5c94d63b70eda6267410478eda406b
rsuresh-coder <82728778+rsuresh-coder@users.noreply.github.com>
2024-08-29 09:25:54 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,62 +1,220 @@
-from typing import Dict, TypedDict, Optional
-from langgraph.graph import StateGraph, START, END
-from PIL import Image
-import io
-from langchain_core.runnables.graph import MermaidDrawMethod
-     
+import re
+import numpy as np
+import openai
+from langchain_core.tools import tool
+import requests
+from datetime import date,datetime
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import END, StateGraph, START
+from langgraph.prebuilt import tools_condition
+from langchain_core.messages import ToolMessage
+from langchain_core.runnables import RunnableLambda
+from langgraph.prebuilt import ToolNode
+from typing import Annotated
+from typing_extensions import TypedDict
+from langgraph.graph.message import AnyMessage, add_messages
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import Runnable, RunnableConfig
+import shutil
+import uuid
+
+from langchain_openai import ChatOpenAI
+
+###### TOOL Utilities
  
-class GraphState(TypedDict):
-    question: Optional[str] 
-    search_results: Optional[str] 
-    response: Optional[str]
+def handle_tool_error(state) -> dict:
+    error = state.get("error")
+    tool_calls = state["messages"][-1].tool_calls
+    return {
+        "messages": [
+            ToolMessage(
+                content=f"Error: {repr(error)}\n please fix your mistakes.",
+                tool_call_id=tc["id"],
+            )
+            for tc in tool_calls
+        ]
+    }
 
-rag_graph = StateGraph(GraphState)
-  
-def handle_search_node(state):
-    question = state.get('question', '').strip()
-    #Perform search here 
-    return {"search_results": ["Result 1 " + question, "Result 2 " + question] }
 
-def handle_generation_node(state):
-    search_results = state.get('search_results')
-    generated = ','.join(search_results)
-    return {"response": generated}
+def create_tool_node_with_fallback(tools: list) -> dict:
+    return ToolNode(tools).with_fallbacks(
+        [RunnableLambda(handle_tool_error)], exception_key="error"
+    )
 
-#Add Nodes
-rag_graph.add_node("search", handle_search_node)
-rag_graph.add_node("generate", handle_generation_node)
 
-#Add Edges
-rag_graph.add_edge('search', "generate")
-rag_graph.add_edge('generate', END)
+def _print_event(event: dict, _printed: set, max_length=1500):
+    current_state = event.get("dialog_state")
+    if current_state:
+        print("Currently in: ", current_state[-1])
+    message = event.get("messages")
+    if message:
+        if isinstance(message, list):
+            message = message[-1]
+        if message.id not in _printed:
+            msg_repr = message.pretty_repr(html=True)
+            if len(msg_repr) > max_length:
+                msg_repr = msg_repr[:max_length] + " ... (truncated)"
+            print(msg_repr)
+            _printed.add(message.id)
 
-#Set Entry point
-rag_graph.set_entry_point("search")
 
-app = rag_graph.compile()
+#FAQ  Ingestion
 
-try:
-    image_bytes = app.get_graph().draw_mermaid_png(draw_method=MermaidDrawMethod.PYPPETEER)
-    image = Image.open(io.BytesIO(image_bytes))
-    image.save("graph.png", "PNG")
-except Exception as e:
-    print(e)
-    pass
-  
-while True:
-    user_input = input("User: ")
-    if user_input.lower() in ["quit", "exit", "q"]:
-        print("Goodbye!")
-        break
-    inputs = {"question": "Hello, good morning"}
-    result = app.invoke(inputs)
-    print(result['response'])
-    # for event in app.stream( {"question" : user_input}):
-    #     # print(event.keys(), event.values())
-    #     for val in event.values():
-    #         if 'response' in val.keys():
-    #             print(val['response'])
-         
-# inputs = {"question": "Hello, good morning"}
-# result = app.invoke(inputs)
-# print(result)
+response = requests.get(
+    "https://storage.googleapis.com/wf_bucket_09152022/wf_qa_pairs.md"
+     
+)
+response.raise_for_status()
+faq_text = response.text
+
+docs = [{"page_content": txt} for txt in re.split(r"(?=\n##)", faq_text)]
+
+
+class VectorStoreRetriever:
+    def __init__(self, docs: list, vectors: list, oai_client):
+        self._arr = np.array(vectors)
+        self._docs = docs
+        self._client = oai_client
+
+    @classmethod
+    def from_docs(cls, docs, oai_client):
+        embeddings = oai_client.embeddings.create(
+            model="text-embedding-3-small", input=[doc["page_content"] for doc in docs]
+        )
+        vectors = [emb.embedding for emb in embeddings.data]
+        return cls(docs, vectors, oai_client)
+
+    def query(self, query: str, k: int = 5) -> list[dict]:
+        embed = self._client.embeddings.create(
+            model="text-embedding-3-small", input=[query]
+        )
+        # "@" is just a matrix multiplication in python
+        scores = np.array(embed.data[0].embedding) @ self._arr.T
+        top_k_idx = np.argpartition(scores, -k)[-k:]
+        top_k_idx_sorted = top_k_idx[np.argsort(-scores[top_k_idx])]
+        return [
+            {**self._docs[idx], "similarity": scores[idx]} for idx in top_k_idx_sorted
+        ]
+
+
+retriever = VectorStoreRetriever.from_docs(docs, openai.Client())
+
+
+@tool
+def lookup_policy(query: str) -> str:
+    """Consult the company policies to check whether certain options are permitted.
+    Use this  performing other 'write' events."""
+    docs = retriever.query(query, k=2)
+    return "\n\n".join([doc["page_content"] for doc in docs])
+
+
+
+#### Agent etc
+ 
+class State(TypedDict):
+    messages: Annotated[list[AnyMessage], add_messages]
+ 
+class Assistant:
+    def __init__(self, runnable: Runnable):
+        self.runnable = runnable
+
+    def __call__(self, state: State, config: RunnableConfig):
+        while True:
+            configuration = config.get("configurable", {})
+            ecn = configuration.get("ecn", None)
+            state = {**state, "user_info": ecn}
+            result = self.runnable.invoke(state)
+            # If the LLM happens to return an empty response, we will re-prompt it
+            # for an actual response.
+            if not result.tool_calls and (
+                not result.content
+                or isinstance(result.content, list)
+                and not result.content[0].get("text")
+            ):
+                messages = state["messages"] + [("user", "Respond with a real output.")]
+                state = {**state, "messages": messages}
+            else:
+                break
+        return {"messages": result}
+ 
+llm = ChatOpenAI(model="gpt-4-turbo-preview")
+
+primary_assistant_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a helpful customer support assistant for Wells Fargo. "
+            " Use the provided tools to search for company policies, and other information to assist the user's queries. "
+            " When searching, be persistent. Expand your query bounds if the first search returns no results. "
+            " If a search comes up empty, expand your search before giving up."
+            "\n\nCurrent user:\n\n{user_info}\n"
+            "\nCurrent time: {time}.",
+        ),
+        ("placeholder", "{messages}"),
+    ]
+).partial(time=datetime.now())
+
+part_1_tools = [
+    TavilySearchResults(max_results=1),
+    lookup_policy,
+   ]
+part_1_assistant_runnable = primary_assistant_prompt | llm.bind_tools(part_1_tools)
+ 
+builder = StateGraph(State)
+ 
+# Define nodes: these do the work
+builder.add_node("assistant", Assistant(part_1_assistant_runnable))
+builder.add_node("tools", create_tool_node_with_fallback(part_1_tools))
+# Define edges: these determine how the control flow moves
+builder.add_edge(START, "assistant")
+builder.add_conditional_edges(
+    "assistant",
+    tools_condition,
+)
+builder.add_edge("tools", "assistant")
+
+# The checkpointer lets the graph persist its state
+# this is a complete memory for the entire graph.
+memory = MemorySaver()
+part_1_graph = builder.compile(checkpointer=memory)
+
+
+
+# Let's create an example conversation a user might have with the assistant
+tutorial_questions = [
+    "Hello, I am Suresh",
+    "When will i receive my first billing statement?",
+    "Who is the head of technology at Wells Fargo?",
+    "What is her salary?",
+    "Who is the CEO of Wells Fargo?",
+    "What is his salary?",
+    "Tell me my name"
+]
+
+# Update with the backup file so we can restart from the original place in each section
+thread_id = str(uuid.uuid4())
+
+config = {
+    "configurable": {
+        # The ecn is used to fetch his/her details 
+        "ecn": "123456789",
+        # Checkpoints are accessed by thread_id
+        "thread_id": thread_id,
+    }
+} 
+
+_printed = set()
+for question in tutorial_questions:
+    print("\n")
+    # print(  '*' * 80)
+    # print("User: ", question, "\n")
+    events = part_1_graph.stream(
+        {"messages": ("user", question)}, config, stream_mode="values"
+    )
+    for event in events:
+        _print_event(event, _printed)
+    # events_list = [ event for event in events]
+    # _print_event(events_list[-1], _printed)
+    # print('*' * 80)
+
